269e8ea86665eb428a349bd6ebe112541ed01d56
Linh Nguyen <linh.nguyen@airthings.com>
2020-07-02 18:06:57 -0500

    Remove extra press enter

--- src/states/GhostInfoState.lua
+++ src/states/GhostInfoState.lua
@@ -14,17 +14,14 @@ function GhostInfoState:render()
     if self.image == gGraphics['ghosts']['adam'] then
         love.graphics.print('Radon does not show any short term effects,', 30, 50)
         love.graphics.print('and is very hard to detect without proper monitoring.', 30, 70)
-        love.graphics.print('[press enter to continue]', 30, 110)
     elseif self.image == gGraphics['ghosts']['bob'] then
         love.graphics.print('It is the number one cause of lung cancer amongst non-smokers.', 30, 50)
         love.graphics.print('Radon induced lung cancer kills more than house fires', 30, 70)
         love.graphics.print('and carbon monoxide combined.', 30, 90)
-        love.graphics.print('[press enter to continue]', 30, 130)
     else
         love.graphics.print('If your Radon levels are significantly high after monitoring', 30, 50)
         love.graphics.print('for a given time period, you should consider calling a', 30, 70)
         love.graphics.print('professional to determine a possible solution.', 30, 90)
-        love.graphics.print('[press enter to continue]', 30, 130)
     end
 
     love.graphics.setFont( gFonts['medium'] )
