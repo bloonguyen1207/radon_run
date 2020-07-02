5507d666668aff25d717b920395dc0dcfcfec6c8
Bloo Nguyen <bloonguyen1207@gmail.com>
2020-07-02 12:01:55 +0200

    Fix project structure

--- main.lua
+++ main.lua
@@ -1,16 +1,4 @@
--- push is a library that will allow us to draw our game at a virtual
--- resolution, instead of however large our window is; used to provide
--- a more retro aesthetic
---
--- https://github.com/Ulydev/push
-push = require'push'
-
--- the "Class" library we're using will allow us to represent anything in
--- our game as code, rather than keeping track of many disparate variables and
--- methods
---
--- https://github.com/vrld/hump/blob/master/class.lua
-Class = require'class'
+require 'src/Dependencies'
 
 -- size of our actual window
 WINDOW_WIDTH = 1280
